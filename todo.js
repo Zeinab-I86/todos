ebad64f324434abf1d4f7aadfef63f428dc2cdb5
Zeinab-I86 <Zeinab.i86@outlook.com>
2024-02-06 00:52:19 +0100

    kopierar Linus kod och försöker göra om den till js-format

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
+import choices from './choices.js';
 
 const todoMenu = () => {
-  console.log(`(
+  console.log(`
            My TODOS!
     What do you want to do?
 Enter the number of your choice!
@@ -16,5 +17,54 @@ Enter the number of your choice!
     0. Exit
 `);
 };
+const runChoice = (choice) => {
+  switch (choice) {
+    case '1':
+      choices.showTodoList();
+      break;
+    case '2':
+      choices.showDoneList();
+      break;
+    case '3':
+      choices.addTodo();
+      break;
+    case '4':
+      choices.addToTop();
+      break;
+    case '5':
+      choices.removeByName();
+      break;
+    case '6':
+      choices.moveItemToDone();
+      break;
+    case '7':
+      choices.moveDown();
+      break;
+    case '8':
+      choices.moveUp();
+      break;
+    case '0':
+      process.exit();
+      break;
+    default:
+      console.log('Please enter a valid number!');
 
+      const main = () => { }
+      while (true) {
+        todoMenu();
+      }
+  }
+}
 
+/*("\nStart meny\n"
+"\n1. Lista av products"
+"\n2. Logga in"     
+match answer.lower():
+case "1":
+print("Lista av products")
+case "2":
+run = self.login_menu()
+case "q":
+self.close_program()
+run = False
+    print("Programet avslutas!")*/
