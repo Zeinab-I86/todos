ac31b18a047bfaaba98b07d4432af1ec3f2fd6b9
Zeinab-I86 <Zeinab.i86@outlook.com>
2024-02-05 23:00:06 +0100

    Lagt in de val jag vill kunna utföra i todo.js

---
+++ todo.js
@@ -0,0 +1,13 @@
+console.log(`(
+  My TODOS!
+  1. Show Todo List 
+  2. Show Done List 
+  3. Add Todo 
+  4. Add Todo To Top 
+  5. Remove Todo By Name 
+  6. Move Todo To Done List 
+  7. Move Todo Down 
+  8. Move Todo Up 
+  0. Exit
+  `);
+  
